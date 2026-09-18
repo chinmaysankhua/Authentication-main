5bca46efc28f17d5fdb1c9c856016c1a0aa1dd70
chinmaysankhua <chinmaysukla510@gmail.com>
2026-09-18 21:07:14 +0530

    login api added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -16,11 +16,13 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     setIsLoading(true)
+    let url;
     if(isLogin){
-
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXhOcSSSkvtZgGp-L0Fg_6nN5JP4iyM-k"
     }else{
-      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXhOcSSSkvtZgGp-L0Fg_6nN5JP4iyM-k",
-
+     url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXhOcSSSkvtZgGp-L0Fg_6nN5JP4iyM-k"
+    }
+     fetch(url,
         {
           method:'POST',
           body:JSON.stringify({
@@ -35,18 +37,22 @@ const AuthForm = () => {
       ).then((res)=>{
         setIsLoading(false)
         if(res.ok){
-
+          return res.json();
         }else{
           return res.json().then((data)=>{
             let errorMessage = "Authentication failed"
             if(data && data.error && data.error.message){
               errorMessage = data.error.message
             }
-            alert(errorMessage)
+            
+            throw new Error(errorMessage)
           })
         }
+      }).then(data=>{
+        console.log(data)
+      }).catch(err=>{
+        alert(err.message)
       })
-    }
   }
 
   return (
